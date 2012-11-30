0906343db6aba0a3ff929e69902b9073699e57cd
Eric Li <eric@drchrono.com>
2012-11-30 12:20:04 -0800

    Global setting

---
+++ Shoji/Shoji/Settings.h
@@ -0,0 +1,14 @@
+//
+//  Settings.h
+//  Shoji
+//
+//  Created by Eric Li on 11/30/12.
+//  Copyright (c) 2012 Dr.Lulu. All rights reserved.
+//
+
+#ifndef Shoji_Settings_h
+#define Shoji_Settings_h
+
+
+
+#endif
